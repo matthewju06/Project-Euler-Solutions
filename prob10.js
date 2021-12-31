1a4b43f64bc7b63401a9a84cd8efe301d5b8fc3e
matthewju06 <matthewju06@gmail.com>
2021-12-31 12:21:37 -0600

    Create prob10.js

---
+++ prob10.js
@@ -0,0 +1,18 @@
+let sum = 0;
+
+for(let currentNum = 2; currentNum < 2000000; currentNum++){
+  if(isPrime(currentNum)){
+    sum += currentNum
+  }
+}
+
+function isPrime(currentNum){ 
+  for(let divisor = 2; divisor <= Math.round(Math.sqrt(currentNum)); divisor++){
+    if(currentNum % divisor === 0){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(sum)
