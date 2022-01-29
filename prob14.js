85c3c4115194c05444bec5d09939684e826acd14
matthewju06 <matthewju06@gmail.com>
2022-01-29 15:18:26 -0600

    Create prob14.js

---
+++ prob14.js
@@ -0,0 +1,22 @@
+let newNum = 0;
+let largestChain = 0
+let largestNum = 0
+
+for(let current = 1; current < 1000000; current++){
+  let chainLength = 0;
+  newNum = current
+  while(newNum !== 1){
+    if(newNum % 2 === 0){
+      newNum = newNum / 2
+      chainLength++
+    }else{
+      newNum = newNum * 3 + 1
+      chainLength++
+    }
+  }
+  if(chainLength > largestChain){
+    largestNum = current
+    largestChain = chainLength
+  }
+}
+console.log(largestNum)
