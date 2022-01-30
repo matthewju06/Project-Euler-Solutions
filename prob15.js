bd81917bea8e8554bb5a5805420541e363d9b137
matthewju06 <matthewju06@gmail.com>
2022-01-29 18:27:50 -0600

    Update prob15.js

--- prob15.js
+++ prob15.js
@@ -1,5 +1,31 @@
-let grid = new Array(20)
+let array =[] 
 
-grid[0][0] = 1
+for(let i = 0; i <= 20; i++){ //Creating a 21x21 2D array, extra 1 on top of 
+                              // 20 to represent 1 at most right and bottom 
+    array[i] = []
+    for(let j = 0; j <= 20; j++){
+        if(i === 20){   // Making bottom row all 1 because if you are at very 
+                        // bottom there is only 1 solution (right) and very
+                        // bottom right is 0 because that is the end meaning 
+                        // nowhere else to go
+            array[i][j] = 1;    
+            array[i][20] = 0;
+        }else if(j === 20){ // Making most right all 1 because if you are at 
+                            // the very right you only have 1 solution (down)
+            array[i][j] = 1;
+        }else{
+        array[i][j]= null;
+        }
+    }
+}
 
-console.log(grid);
+for(i = 19; i >= 0; i--){   // The pattern is if you add the right and bottom 
+                            // value of an unknown point then you can get the
+                            // number of solutions you can go right and down to the end
+    for(j = 19; j >= 0; j--){
+        array[i][j] = array[i+1][j] + array[i][j+1]
+    }
+}
+
+
+console.log(array[0][0]);
