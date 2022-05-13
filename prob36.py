97431936e6d79f42e9dbc72a4573b2c385f867ee
matthewju06 <87710691+matthewju06@users.noreply.github.com>
2022-05-12 19:10:33 -0500

    Upload 5/12/2022

---
+++ prob36.py
@@ -0,0 +1,14 @@
+sum = 0
+
+# Finds if string is palindrome, returns True if true.
+def isPalindrome(string):
+    if string == string[::-1]:
+        return True
+    return False
+
+for num in range(1000000):
+    # Checks if base 10 and base 2 are both palindrome
+    if isPalindrome(str(num)) and isPalindrome(str(bin(num))[2:]):
+        sum += num
+
+print(sum)
