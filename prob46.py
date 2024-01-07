b48654073ca5b45a976c4e76b9bdc77adfcad5b5
matthewju06 <87710691+matthewju06@users.noreply.github.com>
2024-01-07 15:50:33 -0600

    Upload 1/7/2024

---
+++ prob46.py
@@ -0,0 +1,30 @@
+# https://projecteuler.net/problem=45
+# Completed 1/7/2024
+
+import math
+
+def isPrime(num):
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+    for i in range(2,int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
+currentSum = 9
+found = False
+while not found:
+    if not isPrime(currentSum):
+        found = True
+        for possiblePrime in range(2, currentSum - 1):
+            if isPrime(possiblePrime):
+                tempSum = (currentSum - possiblePrime)/2
+                if (math.sqrt(tempSum)).is_integer():
+                    found = False
+                    break
+            
+        if found:
+            print(currentSum)
+    currentSum += 2
